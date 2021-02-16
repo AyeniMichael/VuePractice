2edc2672d319cec13a0be36786d4311f83c0203a
Ayeni Michael <ayenimichael1998@gmail.com>
2021-02-16 14:14:44 +0100

    add initial login app

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-app>
+    <v-app-bar app color="info" dark>
+      <v-toolbar-title><v-img src=""></v-img> Dashboard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text rounded>Home</v-btn>
+      <v-btn text rounded>Login</v-btn>
+    </v-app-bar>
+    <!-- Login Module -->
+    <v-content>
+      <v-card width="400" class="mx-auto mt-5">
+        <v-card-title>
+          <h1 class="display-1">Login</h1>
+        </v-card-title>
+        <v-card-text>
+          <v-form>
+            <v-text-field 
+              label="Username" 
+              prepend-icon="mdi-account-circle" 
+            />
+            <v-text-field
+              type="password" 
+              label="Password"
+              prepend-icon="mdi-lock"
+              append-icon="mdi-eye-off"
+            />
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+            <v-btn color="success">Register</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="info">Login</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-content>
+
+    <v-footer
+        color="primary lighten-1"
+        padless
+      >
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col
+            class="primary lighten-2 py-4 text-center white--text"
+            cols="12"
+          >
+            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+    
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      links: [
+        'Home',
+        'Login'
+      ]
+    }
+  }
+};
+</script>
